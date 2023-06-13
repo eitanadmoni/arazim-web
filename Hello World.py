b2ed680965801b2cc837355e2974c1276eaa0b08
eitanadmoni <eitanadmoni@mail.tau.ac.il>
2023-06-13 18:47:52 +0300

    take name from url adress

--- Hello World.py
+++ Hello World.py
@@ -1,14 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
-# instance of flask application
 app = Flask(__name__)
 
-
-# home route that returns below text when root url is accessed
+# creating route
 @app.route("/")
-def hello_world():
-    return "<p>Hello World</p>"
+def home():
+    return render_template("index.html")
 
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
